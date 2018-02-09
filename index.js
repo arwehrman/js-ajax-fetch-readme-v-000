fe42c922cd3688b3d2a24d6ea24ae7ec105016e9
Amanda Wehrman <arwehrman@gmail.com>
2018-02-09 00:39:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+
+const token = 'e128dee41b0bb4573f8b717debd615de47c4d1e5'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
